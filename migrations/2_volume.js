43bd9f09540dc643432c0994d13a2243e008a1a7
Timo <tklaasee@gmail.com>
2021-06-14 13:01:30 +0200

    Added a dummy test wallet to act as escrow

--- migrations/2_volume.js
+++ migrations/2_volume.js
@@ -1,8 +1,9 @@
 const Big = require('big-js');
 Big.DP = 40;
 
-const Volume = artifacts.require('Volume');
+const Volume = artifacts.require('TestVolume');
 
 module.exports = function (deployer) {
-    deployer.deploy(Volume);
+    // NOTE - Make this address the address for the deployed escrow
+    deployer.deploy(Volume, '0xe420279D0bf665073f069cB576c28d6F77633b20'); 
 }
