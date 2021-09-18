feedd9e897940d7936d854a1c99708680cb2668b
humanshield85 <rachidboudjelida@gmail.com>
2021-09-18 10:59:22 +0100

    Better Migration logic

--- migrations/4_Faucet.js
+++ migrations/4_Faucet.js
@@ -1,7 +1,7 @@
-const Volume = artifacts.require("TestVolume"); // TODO change this in production
+const TestVolume = artifacts.require("TestVolume");
 const VolumeFaucet = artifacts.require("VolumeFaucet");
 
 module.exports = function (deployer, network, accounts) {
     if (network == "kovan" || network == "BSCTest")
-        deployer.deploy(VolumeFaucet, accounts[0], Volume.address)
+        deployer.deploy(VolumeFaucet, accounts[0], TestVolume.address)
 };
